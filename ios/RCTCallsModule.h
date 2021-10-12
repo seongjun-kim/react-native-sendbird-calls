29e7246b47ed8dca2076fd9e4f4a637d4b11ff11
seongjun_kim <lewisel@naver.com>
2021-10-12 16:56:03 +0900

    chore: init project with create-react-native-library

---
+++ ios/RCTCallsModule.h
@@ -0,0 +1,13 @@
+//
+//  RCTCallsModule.h
+//  SendbirdCalls
+//
+//  Created by 김성준 on 2021/10/12.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#ifndef RCTCallsModule_h
+#define RCTCallsModule_h
+
+
+#endif /* RCTCallsModule_h */
